1adc703767bd4a7cb5f9c834858ddd37f0c077c2
jeffersonveloz-lab <jeffersonvelozh@gmail.com>
2026-05-26 17:03:54 -0500

    pequeños ajustes

--- Frontend/src/app/feactures/pages/private/login-component/login-component.ts
+++ Frontend/src/app/feactures/pages/private/login-component/login-component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login-component',
-  imports: [],
+  imports: [MatIconModule, MatCardModule, MatFormFieldModule, MatInputModule,
+    MatButtonModule, MatCheckboxModule],
   templateUrl: './login-component.html',
   styleUrl: './login-component.scss',
 })
